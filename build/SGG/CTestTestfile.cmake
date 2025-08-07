144d04a5fc19fb2e10d0b9f8a986e5bacae3fe57
yangjunwon1309 <yangjunwon1309@gmail.com>
2025-08-07 15:38:21 +0900

    vla_bringup crash avoided

--- build/SGG/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/junwon/CMU-VLA-Challenge/ai_module/src/SGG
-# Build directory: /home/junwon/CMU-VLA-Challenge/ai_module/build/SGG
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
